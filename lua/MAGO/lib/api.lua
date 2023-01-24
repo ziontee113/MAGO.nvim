b2487b7b61f62290e0412c972b89e9d30b2234f2
ziontee113 <ziontee113@gmail.com>
2023-01-24 19:55:11 +0700

    feat(api): add equivalent()

--- lua/MAGO/lib/api.lua
+++ lua/MAGO/lib/api.lua
@@ -1,7 +1,17 @@
 local M = {}
 
-M.something = function()
-    -- TODO:
+M.equivalent = function()
+    local path = vim.api.nvim_buf_get_name(0)
+    local path_split = vim.split(path, "/")
+
+    local index = 7
+    if path_split[index] == "lua" then
+        path_split[index] = "tests"
+    elseif path_split[index] == "tests" then
+        path_split[index] = "lua"
+    end
+
+    return table.concat(path_split, "/")
 end
 
 return M
--- tests/MAGO/lib/api_spec.lua
+++ tests/MAGO/lib/api_spec.lua
@@ -1,6 +1,6 @@
--- local api = require("MAGO.lib.api")
+local api = require("MAGO.lib.api")
 
-describe("MAGO api module", function()
+describe("vim.api.nvim_buf_get_name(0)", function()
     local compare_path = function(path)
         local cmd = "e " .. path
         vim.cmd(cmd)
@@ -11,16 +11,40 @@ describe("MAGO api module", function()
         return vim.split(buffer_path, "/")
     end
 
-    it("get correct path from buffer, inside `lua` folder", function()
+    it("get correct path from buffer, file inside `lua` folder", function()
         local path = "/home/ziontee113/.config/dev-nvim/MAGO/lua/MAGO/lib/api.lua"
         local buffer_path_split = compare_path(path)
 
         assert.equal("lua", buffer_path_split[7])
     end)
-    it("get correct path from buffer, inside `tests` folder", function()
+    it("get correct path from buffer, file inside `tests` folder", function()
         local path = "/home/ziontee113/.config/dev-nvim/MAGO/tests/MAGO/lib/api.lua"
         local buffer_path_split = compare_path(path)
 
         assert.equal("tests", buffer_path_split[7])
+    end)
+end)
+
+describe("MAGO's api's equivalent()", function()
+    local compare_path = function(path, equivalent)
+        local cmd = "e " .. path
+        vim.cmd(cmd)
+
+        local buffer_equivalent = api.equivalent()
+
+        assert.equal(buffer_equivalent, equivalent)
+    end
+
+    it("works if file in `lua` folder", function()
+        local path = "/home/ziontee113/.config/dev-nvim/MAGO/lua/MAGO/lib/api.lua"
+        local equivalent = "/home/ziontee113/.config/dev-nvim/MAGO/tests/MAGO/lib/api.lua"
+
+        compare_path(path, equivalent)
+    end)
+    it("works if file in `tests` folder", function()
+        local path = "/home/ziontee113/.config/dev-nvim/MAGO/tests/MAGO/lib/api.lua"
+        local equivalent = "/home/ziontee113/.config/dev-nvim/MAGO/lua/MAGO/lib/api.lua"
+
+        compare_path(path, equivalent)
     end)
 end)
