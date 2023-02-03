e4619f8837b7adde0f57eb1a818cff843e889dbf
ziontee113 <ziontee113@gmail.com>
2023-02-04 05:12:05 +0700

    Remove old demonstrations

--- tests/MAGO/lib/luastring_converter_spec.lua
+++ tests/MAGO/lib/luastring_converter_spec.lua
@@ -29,32 +29,4 @@ Some other day is not something beautiful.]]
         assert.are.same(want, got)
         assert.equals("Every day is a beautiful day.\nSome other", input:sub(got[1], got[2]))
     end)
-
-    it("multi line vs single line demonstration", function()
-        local expected =
-            "poem:\nEvery day is a beautiful day.\nSome other day is not something beautiful."
-
-        assert.are.same(expected, input)
-    end)
-
-    it("string:sub() demonstration", function()
-        assert.are.same("poem", input:sub(1, 4))
-
-        -- \n is 1 single character, hence 7 not 8 to start `Every`
-        assert.are.same("Every", input:sub(7, 11))
-    end)
-
-    it("string:find() demonstration", function()
-        local find_start, find_end = input:find("day")
-        assert.equal(find_start, 13)
-        assert.equal(find_end, 15)
-
-        local find_start, find_end = input:find("day", 15)
-        assert.equal(find_start, 32)
-        assert.equal(find_end, 34)
-
-        local find_start, find_end = input:find("day", 34)
-        assert.equal(find_start, 48)
-        assert.equal(find_end, 50)
-    end)
 end)
